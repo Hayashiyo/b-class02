80eb2ecc6936014c3c866c0bddc92cefdb021428
unknown <shiyolinox@gmail.com>
2023-01-09 11:37:31 +0800

    basic

---
+++ front/login.php
@@ -0,0 +1,56 @@
+<fieldset class="ct">
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td>acc</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>pw</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>
+                <button onclick="login()">login</button>
+                <button onclick="cl()">delete</button>
+            </td>
+            <td>
+                <a href="do=forgot">忘記密碼</a>
+                <a href="do=reg">註冊</a>
+            </td>
+        </tr>
+    </table>
+</fieldset>
+
+<script>
+    function cl() {
+        $("#acc,#pw").val('')
+
+    }
+
+    function login() {
+        let user = {
+            acc: $("#acc").val(),
+            pw: $("#pw").val()
+        }
+        $.post("./api/chk_acc.php", user, (result) => {
+                if (parseInt(result) === 1) {
+                    $.post("./api/chk_pw.php", user, (result) => {
+                            if (parseInt(result) === 1) {
+                                if(user.acc==='admin'){
+                                    location.href='back.php'
+                                }else{
+                                    location.href='index.php'
+                                }
+                            }else{
+                                alert("wrong pw")
+
+                            }
+                        })
+                    }
+                    else {
+                        alert("no acc");
+                    }
+                })
+        }
+</script>
--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 <script src="./js/jquery-1.9.1.min.js"></script>
 <script src="./js/js.js"></script>
 </head>
-
+<?php
+include"./api/base.php";
+?>
 <body>
 <div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
 	<pre id="ssaa"></pre>
@@ -15,9 +17,9 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+        <?=date("m月d號 l");?> | 今日瀏覽: <?=$Total->find(['date'=>date("Y-m-d")])['total'];?> | 累積瀏覽: <?=$Total->sum('total');?>  <a href="./index.php" style="float:right">回首頁</a>       </div>
         <div id="title2">
-        	
+        	<a href="./index.php"><img src="./02B01.jpg" alt="" title="回首頁"></a>
         </div>
         <div id="mm">
         	<div class="hal" id="lef">
@@ -29,19 +31,29 @@
                	                 </div>
             <div class="hal" id="main">
             	<div>
-            		
+            		<span style="width:78%; display:inline-block;"><marquee behavior="asd" direction="asd">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地! 詳見最新文章</marquee></span>
                 	<span style="width:18%; display:inline-block;">
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
                     	<div class="">
+							<?php
+							$do=$_GET['do']??'home';
+							$file="./front/".$do.".php";
+							if(file_exists($file)){
+								include $file;
+							}else{
+								include "./front/home.php";
+							}
+
+							?>
                 		                        </div>
                 </div>
             </div>
         </div>
         <div id="bottom">
-    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
+    	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2023健康促進網社群平台 All Right Reserved 
     		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
     </div>
 
